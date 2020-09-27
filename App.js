2e2cf705f7ef80de7fc736fd0e8b62b6392e0ba2
tresor-ruph <tresortek8@gmail.com>
2020-09-27 22:26:42 +0200

    homepage

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <View style={styles.container}>
+    <View style={styles.box1}>
+      <Text style={styles.text}>Demande de Service</Text>
     </View>
-  );
-}
+    <View style={styles.box2}>
+      <Text style={styles.text}>Offre de Service</Text>
+
+    </View>
+    <View style={styles.box3}>
+      <Text style={styles.text}>Espace personnel</Text>
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  box1: {
+    position: 'absolute',
+    top: 250,
+    left: 40,
+    width: 150,
+    height: 150,
+    backgroundColor: 'rgba(242,100,107,1)',
+    borderRadius : 20,
+  },
+  box2: {
+    ...StyleSheet.absoluteFill,
+    width: 150,
+    height: 150,
+    top: 250,
+    left: 200,
+    backgroundColor: 'rgba(94,94,205,1)',
+
+    borderRadius : 20,
+  },
+  box3: {
+    position: 'absolute',
+    top: 420,
+    left: 40,
+    width: 310,
+    height: 150,
+    backgroundColor: 'rgba(0,128,0,0.5)',
+
+    borderRadius : 20,
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 40,
+  }
 });
+
+export default App;
