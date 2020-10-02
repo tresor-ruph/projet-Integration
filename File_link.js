0c9d986cb812cae15ad5ad2be3cdd7810639d9b6
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-10-02 20:07:47 +0200

    Ajout page nav vers demande service

--- File_link.js
+++ File_link.js
@@ -4,8 +4,11 @@ import HomeScreen from './Homescreen/HomeScreen'
 import Chat  from './Messenger/Chat'
 import MainChat  from './Messenger/Main_Chat'
 import catalogueOffre from './Catalogue/catalogueOffre'
+import Demande from './DemandeFormulaire/Demande'
+import ListeDem from './Catalogue/ListeDem'
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 
 // Create the navigator
@@ -18,8 +21,9 @@ render() {
             <Stack.Navigator>
             <Stack.Screen name = "HomeScreen" component = {HomeScreen} />
             <Stack.Screen name = "MainChat" component = {MainChat} />
-            <Stack.Screen name = "catalogueOffre" component = {catalogueOffre} />
+            <Stack.Screen name = "FaireDemande" component = {Demande} />
             <Stack.Screen name = "Chat" component = {Chat} />
+            <Stack.Screen name = "ListeDem" component = {ListeDem} />
 
             </Stack.Navigator>
         </NavigationContainer>
