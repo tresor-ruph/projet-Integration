5f91e9f76919bd3fe936806e8f0a251fc1d87299
HE201559 <56299123+HE201559@users.noreply.github.com>
2020-10-02 20:05:49 +0200

    Rajout css / bouton envoie formulaire

--- demandeFormulaire/Demande.js
+++ demandeFormulaire/Demande.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, TextInput, CheckBox } from 'react-native'
+import {StyleSheet, View, TextInput, CheckBox,TouchableOpacity, Text, Alert } from 'react-native'
 import { Title, Paragraph } from 'react-native-paper';
 
 
@@ -7,11 +7,12 @@ class Demande extends React.Component{
   render(){
     return (
       <View style={styles.container}>
-        <Title style={{height:90}}> Formulaire de demande de service</Title>
-        <Paragraph> Disponibilités </Paragraph>
-        <TextInput multiline={true} numberOfLines={3} style={{marginBottom : 30, width : 300}} placeholder="Entrez vos heures de disponibilités de la journée"/>
-        <Paragraph> Descriptif </Paragraph>
-        <TextInput multiline={true} numberOfLines={5} style={{ width : 300}} placeholder="Entrez un descriptif du service"/>
+        <Title style={{height:100, fontSize:30, textAlign: 'center'}}> Formulaire de demande de service</Title>
+        <Paragraph style={styles.para}> Descriptif </Paragraph>
+        <TextInput multiline={true} numberOfLines={5} style={styles.desc} placeholder="Entrez un descriptif du service"/>
+        <TouchableOpacity style={styles.bout} onPress={() => Alert.alert('Appuyé')}>
+          <Text style={styles.textBout}> Envoyer votre demande </Text>
+        </TouchableOpacity>
       </View>
     );
   };
@@ -21,10 +22,48 @@ class Demande extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginLeft:20,
     justifyContent: "center",
+    backgroundColor: 'rgba(106,106,255,0.6)',
   },
+
+  bout: {
+    width: 200,
+    height : 150,
+    backgroundColor: 'rgba(242,100,107,1)',
+    borderRadius:20,
+    marginTop:150,
+    left:80,
+  },
+
+  para:{
+    height:50,
+    left : 40,
+    fontSize: 20,
+    marginTop:40,
+    fontWeight :'bold',
+  },
+
+  textBout: {
+    color: '#FFF',
+    fontSize: 25,
+    textAlign: "center",
+    marginTop:45,
+  },
+
+  desc:{
+    position:'absolute',
+    height : 110,
+    width : 300,
+    left:30,
+    fontSize:15,
+    fontWeight :'bold',
+    borderWidth: 2,
+    borderColor: "#20232a",
+    textAlign: "center",
+  }
 });
+
+
 
 
 
