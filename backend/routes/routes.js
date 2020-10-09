87e21368ed093800a87f6538d63a66b9efeeefaa
JoachimSGL <56150685+JoachimSGL@users.noreply.github.com>
2020-10-09 14:55:24 +0200

    ajout backend

---
+++ backend/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const cont = require('../controllers/auth');
+router.get("/", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+  });
+
+router.post('/auth',cont.register);
+
+
+module.exports = router;
