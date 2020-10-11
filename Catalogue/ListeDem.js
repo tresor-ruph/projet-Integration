621c6de0286bbc401a8d638caf645bad0d79ae85
Bouquoyoue Bilel <HE201666@students.ephec.be>
2020-10-11 19:27:32 +0200

    debut catalogue

--- Catalogue/ListeDem.js
+++ Catalogue/ListeDem.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import {Title} from 'react-native-paper';
 import {View} from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 
 
 class ListeDem extends React.Component {
+  state = { demande : [
+    { Titre: 'Salim', Description: 'Lave-vaisselle' },
+    { Titre: 'Bilel', Description: 'Courir' },
+    { Titre: 'Adel', Description: 'Sortir' },
+    { Titre: 'Souhaib', Description: 'Sourire' },
+    { Titre: 'Florian', Description: 'Courses' },
+  ]}
   render(){
     return(
-        <View>
-          <Title> Liste des demandes </Title>
-        </View>
+      <View>
+      {
+        this.state.demande.map((l, i) => (
+          <ListItem key={i} bottomDivider>
+            <Avatar source={{uri: l.avatar_url}} />
+            <ListItem.Content>
+              <ListItem.Title>{l.Titre}</ListItem.Title>
+              <ListItem.Subtitle>{l.Description}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        ))
+      }
+    </View>
     );
   };
 };
