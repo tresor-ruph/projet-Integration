93c528d89c7b5beb11b2b65519f599137c6bf8e4
Bouquoyoue Bilel <HE201666@students.ephec.be>
2020-10-18 23:12:03 +0200

    backend envoyer demande fait

--- backend/controller/ajoutDem.js
+++ backend/controller/ajoutDem.js
@@ -1,14 +1,37 @@
-const mysql = require("mysql");
-const db = mysql.createConnection({
-    host: 'sql7.freemysqlhosting.net' ,
-    user: 'sql7343279',
-    password: 'lm5ksRt97g',
-    database: 'sql7343279'
+const demande = require('../model/demande.model')
+// find a all contacts
+
+exports.findAll = (req,res) => {
+  console.log('test1')
+  demande.findDemande( (err, data) => {
+      if (err) {
+            res.status(500).send({
+              message: "Error retrieving * demande "
+            });
+          }
+        else {
+          res.header("Access-Control-Allow-Origin","*");
+          res.send(data);
+        }
+  });
+};
+
+
+exports.create = (req, res) => {
+  const demande3 = {
+    userName: req.body.userName,
+    descriptif: req.body.descriptif,
+    categorie: req.body.categorie
+  };
+  demande.createDemande(demande3)( (err, data) => {
+    if (err) {
+          res.status(500).send({
+            message: "Error creating demande "
+          });
+        }
+      else {
+        res.header("Access-Control-Allow-Origin","*");
+        res.send(data);
+      }
 });
-
-exports.registerDem = (req,res) =>{
-    console.log(req);
-    const { idDemande , userId , Titre , Description, Statut } = req.body;
-    db.query('INSERT INTO Utilisateurs (userId , Titre , Description, Statut) VALUES ?',[idDemande , userId , Titre , Description, Statut])
-
-}
+};
--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -3,16 +3,16 @@ const contacts = require('../model/user.model')
 // find a all contacts
 
 exports.findAll = (req,res) => {
-contacts.findContacts( (err, data) => {
-    if (err) {
-          res.status(500).send({
-            message: "Error retrieving * users "
-          });
+  contacts.findContacts( (err, data) => {
+      if (err) {
+            res.status(500).send({
+              message: "Error retrieving * users "
+            });
+          }
+        else {
+          res.header("Access-Control-Allow-Origin","*");
+          res.send(data);
         }
-       else {
-        res.header("Access-Control-Allow-Origin","*");
-        res.send(data);
-      }
-});
+  });
 };
 
