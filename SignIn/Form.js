9b1ff0e288679505dfd9a81acd297d5c6c9d04d4
sebastien <he201693@students.ephec.be>
2020-10-20 00:10:59 +0200

    css + hide on/off mdp + 2 alert

--- SignIn/Form.js
+++ SignIn/Form.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import { StyleSheet, View, Button, TextInput, ScrollView, Switch, Text } from "react-native";
 class Form extends React.Component {
     constructor() {
       super();
@@ -8,17 +8,51 @@ class Form extends React.Component {
         nom:'',
         prenom:'',
         motdepasse:'',
+        repMotdepasse:'',
         adresse: '',
-        codePostal: '',
-        Mail: ''
+        dateNaissance: '',
+        mail: '',
+        showPassword: true,
       }
+      //sert ds la visualisation du mdp
+      this.toggleSwitch = this.toggleSwitch.bind(this);  
     }
+    //sert ds la visualisation du mdp
+    toggleSwitch() {
+      this.setState({ showPassword: !this.state.showPassword });
+    }
+
     submit() {
-      console.log(this.state)
+      //envoie msg d'erreur si un champ est encore vide
+      if(this.state.nom == '' || this.state.prenom == '' || this.state.motdepasse == '' || this.state.repMotdepasse == '' || this.state.adresse == '' || this.state.dateNaissance == '' || this.state.mail == '') {
+        let simpleAlertHandler = () => {
+          alert("Tous les champs ne sont pas remplis !");
+        };
+        simpleAlertHandler();
+        console.log(this.state);
+        return;
+      }
+      //envoie msg d'erreur si mdp répété est différent
+      if(this.state.motdepasse != this.state.repMotdepasse) {
+        let simpleAlertHandler = () => {
+          alert("Le mot de passe n'est pas correctement répété !");
+        };
+        simpleAlertHandler();
+        console.log(this.state + ' oui');
+        return;
+      }
 
       fetch('http://localhost:8080/auth/', {
         method: 'POST',
         body: JSON.stringify(
+          /* c'est mieux ceci qui est en commentaire car le state contient des elements inutiles à la DB
+          nom: this.state.nom,
+          prenom: this.state.prenom,
+          adresse: this.state.adresse,
+          dateNaissance: this.state.dateNaissance,
+          mail: this.state.mail,
+          password: this.state.motdepasse
+          */
           this.state
         ),
         headers: {
@@ -36,45 +70,91 @@ class Form extends React.Component {
     }
 
     render() {
-      return (
-        <View>
-          <TextInput
-            placeholder="nom"
-            onChangeText={(text)=> { this.setState({ nom: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <TextInput
-            placeholder="prénom"
-            onChangeText={(text)=> { this.setState({ prenom: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <TextInput
-            placeholder="adresse"
-            onChangeText={(text)=> { this.setState({ adresse: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <TextInput
-            placeholder="code postal"
-            onChangeText={(text)=> { this.setState({ codePostal: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <TextInput
-            placeholder="Mail"
-            onChangeText={(text)=> { this.setState({ Mail: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <TextInput
-            placeholder="mot de passe"
-            secureTextEntry={true}
-            onChangeText={(text)=> { this.setState({ motdepasse: text }) }}
-            style={{borderWidth:1,borderColor:'blue', margin:20}}
-          ></TextInput>
-          <Button
-            title="s'incrire"
-            onPress={()=>{this.submit()}}
-          ></Button>
-        </View>
+      return ( 
+        <ScrollView>
+          <View style={styles.container}>
+            <TextInput
+              placeholder="Nom"
+              onChangeText={(text)=> { this.setState({ nom: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Prénom"
+              onChangeText={(text)=> { this.setState({ prenom: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Adresse"
+              onChangeText={(text)=> { this.setState({ adresse: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Date de naissance"
+              onChangeText={(text)=> { this.setState({ dateNaissance: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Adresse email"
+              onChangeText={(text)=> { this.setState({ mail: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Mot de passe"
+              secureTextEntry={this.state.showPassword}
+              onChangeText={(text)=> { this.setState({ motdepasse: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <TextInput
+              placeholder="Répétition du mot de passe"
+              secureTextEntry={this.state.showPassword}
+              onChangeText={(text)=> { this.setState({ repMotdepasse: text }) }}
+              style={styles.textInput}
+            ></TextInput>
+            <Text style={styles.text}>
+              Cliquer pour afficher les mots de passe
+            </Text>
+            <Switch
+              onValueChange={this.toggleSwitch}
+              value={!this.state.showPassword}
+              style={styles.switch}
+            />
+            <Button
+              title="S'inscrire"
+              onPress={()=>{this.submit()}}
+            ></Button>
+          </View>
+        </ScrollView>
       )
     }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+    backgroundColor: "#eaeaea",    
+  },
+
+  textInput: {
+   marginBottom: 25,
+   borderWidth:1,
+   borderColor:'blue',
+   borderRadius: 10,
+   textAlign: "center",
+   height: 45,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  switch: {
+    transform:[{ scaleX: 1.5 }, { scaleY: 1.5 }],
+    marginRight: 120,
+    marginBottom: 40,
+    marginTop: 10,
+  }
+
+})
+
 export default Form;
