f4ff902c2c126d4a29dfda3275ed62b1d3e53373
PATRICKTCHOUPE2TL1 <patricktchoupe85@gmail.com>
2020-10-26 16:48:04 +0100

    test d'integration de l'api

--- Profil/Server/model/db.js
+++ Profil/Server/model/db.js
@@ -1,32 +1,14 @@
 /* eslint-disable quotes */
+// eslint-disable-next-line import/no-unresolved
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config");
 
-let connection;
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+});
 
-function handleDisconnect() {
-  connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
-  });
-  connection.connect((err) => {
-    if (err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);
-    } else { console.log("Connected !"); }
-  });
-  connection.on('error', (err) => {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+module.exports = pool;
