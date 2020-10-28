9af86333553928151a66bf3a63a9d8ac8b8a73a7
Degives <HE201559@students.ephec.be>
2020-10-28 18:00:17 +0100

    style formulaire

--- DemandeFormulaire/Demande.js
+++ DemandeFormulaire/Demande.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {StyleSheet, View, TextInput,TouchableOpacity, Text, Alert, Picker } from 'react-native'
 import { Title, Paragraph } from 'react-native-paper';
-import axios from 'axios'
+
 
 
 
@@ -27,11 +27,11 @@ class Demande extends React.Component{
   
    submit(){
   
-     console.log(this.state) 
+     //console.log(this.state) 
      const newDemande = {userName: this.state.userName, categorie: this.state.categorie, descriptif: this.state.descriptif}
      console.log(newDemande);
 
-     fetch('http://localhost:3000/demandeE/', {
+     fetch('http://localhost:3000/demandeE/', {  // C'est quoi demandeE poulet ?
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -50,11 +50,11 @@ class Demande extends React.Component{
     return (
       <View style={styles.container}>
         
-        <Title style={{height:100, fontSize:30, textAlign: 'center'}}> Formulaire de demande de service</Title>
+        <Title style={{height:70, fontSize:25, textAlign: 'center', marginTop:'10%'}}> Formulaire de demande de service</Title>
 
         <Paragraph style={styles.para}> Type de demande</Paragraph>
 
-          <Picker selectedValue = {this.state.categorie}  onValueChange = {this.updateCategorie}>
+          <Picker style={{height:100, fontSize:25, width:'90%', marginLeft:'5%', marginTop:'5%'}} selectedValue = {this.state.categorie}  onValueChange = {this.updateCategorie}>
               <Picker.Item label="Aller faire des courses" value="courses" />
               <Picker.Item label="Aller chercher un colis" value="colis" />
               <Picker.Item label="Aller chercher les enfants" value="Enfants" />
@@ -62,7 +62,7 @@ class Demande extends React.Component{
               <Picker.Item label="Autres" value="Autres" />
           </Picker>
          
-          <TextInput multiline={true} numberOfLines={5} onChangeText={(text)=>{this.setState({descriptif:text})}} style={styles.desc} placeholder="Entrez un descriptif du service"/>
+          <TextInput mode='outlined' multiline={true} numberOfLines={5} onChangeText={(text)=>{this.setState({descriptif:text})}} style={styles.desc} placeholder="Entrez un descriptif du service"/>
 
           <TouchableOpacity style={styles.bout} onPress={() => {this.submit()}}>
 
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-start",
-    backgroundColor: 'rgba(106,106,255,0.6)',
+    backgroundColor: 'rgb(230,230,230,1)',
   },
 
   listD: {
@@ -94,33 +94,31 @@ const styles = StyleSheet.create({
   bout: {
     width: '98%',
     height : '20%',
-    backgroundColor: 'rgba(242,100,107,1)',
+    backgroundColor:'rgb(0, 150, 136)',
     borderRadius:20,
-    marginTop:'80%',
+    marginTop:'62%',
     marginLeft: '1%', 
   },
 
   para:{
-    fontSize:20,
-    fontWeight:"bold",
-    marginTop: '5%'
+    fontSize:25,
+    marginLeft:'8%',
   },
   textBout: {
     color: '#FFF',
     fontSize: 35,
     textAlign: "center",
-    marginTop: '7%',
+    marginTop: '11%',
   },
 
   desc:{
     flex:1,
     position:'absolute',
-    height : '35%',
+    height : '30%',
     width : '90%',
     marginLeft: '5%',
-    marginTop: '45%',
-    fontSize: 40,
-    fontWeight :'bold',
+    marginTop: '62%',
+    fontSize: 30,
     borderWidth: 2,
     borderColor: "#20232a",
     textAlign: "center",
