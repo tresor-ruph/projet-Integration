820f067ecadb9b6d1166d83f4f9a26cf46028e45
JoachimSGL <56150685+JoachimSGL@users.noreply.github.com>
2020-10-28 16:17:10 +0100

    backend réorganisé

--- backend/server.js
+++ backend/server.js
@@ -40,141 +40,15 @@ db.connect( (err) =>{
 
 
 
-var sessionMidleware = {secret: "its a secret!",saveUninitialized: true,resave: false};
+//var sessionMidleware = {secret: "its a secret!",saveUninitialized: true,resave: false};
 
 
 //app.use(session({unset: 'keep',secret: "its a secret!",saveUninitialized: true,resave: true,cookie: { secure: false,maxAge: 60000 }}));
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(bodyParser.json());
-app.use(session(sessionMidleware));
-//app.use('/auth',require('./routes/au'));
-app.get('/Mail', (req,res) =>{
-  var rechsql = 'SELECT Mail from Utilisateurs';
-  var spotted = '';
-  var Mail = req.param('Mail',null);
-  db.query(rechsql, function (err, result, fields) {
-    //if (err) {throw err;}else{
-    
-    for(var i = 0;i<result.length;i++){
-      if(result[i].Mail==Mail){
-          spotted = result[i].Mail;
-          return res.status(400).json({
-            status: 'error',
-            error: 'email deja utilisé',
-          });
-          //res.json({message: "email deja utilisé"});
-      }
-  }
-  res.json({message: 'ok'});
-})
-  });
-app.post('/auth',(req, res) => {
-
-  
-	  var nom = req.body.nom;
-    var prenom = req.body.prenom;
-    var adresse = req.body.adresse;
-    var codePostal = req.body.codePostal;
-    var Mail = req.body.mail;
-    var password = req.body.password;
-    
-
-	
-
-    /*
-   const saltRounds = 10;
-   const myPlaintextPassword = password;
-   var h = '';
-
-   bcrypt.genSalt(saltRounds, function(err, salt) {
-     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
-         console.log(hash);
-         h=hash;
-     });
-   });
-
-*/
-
-
-var rechsql = 'SELECT Mail from Utilisateurs';
-  var spotted = true;
-  db.query(rechsql, function (err, result, fields) {
-    
-    for(var i = 0;i<result.length;i++){
-      if(result[i].Mail==Mail){
-          spotted = false;
-          /*
-          res.status(400).json({
-            status: 'error',
-            error: 'email deja utilisé',
-            message: 'error' 
-          });*/
-      }
-  }
-if(spotted){
-
-  var sql = 'INSERT INTO Utilisateurs(Nom , Prenom , Adresse , CodePostal,Mail,password) VALUES ? ';
-  var values = [[nom,prenom,adresse,codePostal,Mail,password]];
-  db.query(sql,[values]);
-  //req.session.email = Mail;
-  var rechid = 'SELECT Id from Utilisateurs where Mail = "'+Mail+'"';
-  db.query(rechid, function (err, resu, fields) {
-    console.log(resu);
-  if(resu == undefined){
-    res.json({ message: 'erreur interne'});
-  }else{
-    res.json({ message: 'inscription finie', id : resu[0].Id });
-  }
-  });
-
-}else{
-  res.json({ message: 'error' });
-}
-
-})
-
-
-
-      
-  });
-
-  
-
-  app.post('/login',(req,res) => {
-    var password = req.body.password;
-    var Mail = req.body.mail;
-
-    
-    
-    var sql = 'SELECT * from Utilisateurs where Mail = "'+Mail+'"';
-    db.query(sql, function (err, result, fields) {
-if(result[0] == undefined){
-  res.json({message: 'erreur'});
-}else{
-      
-/*
-bcrypt.compare(result[0].password, password, function(err, re) {
-  console.log(result[0].password);
-  console.log(re);
-  
-  */
-
-      //if(re){
-        //req.body.session = Mail;
-        //req.session.email = Mail;
-        //console.log(req.session);
-        
-        res.json({message: "entrée dans l'appli" , id : result[0].Id, hash: result[0].password});
-        //return res.redirect(307,'/appli');
-      //}else{
-        //res.json({message: 'erreur'});
-      //}
-    
-//});
-
-}
-  })
-  });
+//app.use(session(sessionMidleware));
+app.use('/auth',require('./routes/au'));
+app.use('/login',require('./routes/log'));
 
 
 
@@ -183,8 +57,7 @@ bcrypt.compare(result[0].password, password, function(err, re) {
 
 
 
-
-  app.use((req,res,next)=>{
+  app.post('/appli',(req,res,next)=>{
     console.log('()((()()()()()()(()))))');
     if(!req.body.id){
       const err = new Error("nope");
@@ -197,21 +70,8 @@ bcrypt.compare(result[0].password, password, function(err, re) {
 
 
 
-  app.post('/appli',(req,res)=>{
-    var sql = 'SELECT * from Utilisateurs where id = "'+req.body.id+'"';
-    db.query(sql, function (err, result, fields) {
-      if(result[0] == undefined){
-        res.json({message: 'erreur'});
-      }else{
-          if(req.body.id){
-            res.json({ message: "entrée dans l'appli" , utilisateur: result[0].Mail });
-          }else{
-            res.json({ message: "vous n'êtes pas autorisé à etre ici" });
-          }
-  }});
-
-  });
-
+  app.use('/appli',require('./routes/appli'));
+ 
 
 /*
 
